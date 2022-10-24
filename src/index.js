962fa84665f2d4eb5bc0fb3102036c8e3806a804
bozzhik <bozzhik@ya.ru>
2022-10-24 23:50:31 +0300

    script index[zagluska]

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import './index.css'
 
 // index.html shortcut animation change
 
+const texts = ['ctrl+f', 'cmd+c', 'ctrl+v', 'cmd+a', 'cmd+z', 'cmd+s', 'cmd+d'];
+const textElement = document.querySelector('.shortcut-text');
 
+function changeText(idx) {
+    textElement.innerText = texts[idx];
+
+    setTimeout(() => {
+        if (idx + 1 === texts.length) {
+            changeText(0);
+            return
+        }
+
+        changeText(idx + 1)
+    }, 500); // 1 second
+}
+
+changeText(0);
 
 // index.html form script
 
