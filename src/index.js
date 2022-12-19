f14351a792627cc1542808c26b91a79a4d3c2016
bozzhik <bozzhik@ya.ru>
2022-12-19 18:54:32 +0300

    modal window

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './index.css';
 
+// modal window
+import './javascript/modal.js';
+
+// shortcut change animation
 import './javascript/shortcut.js';
+
+// form done script
 import './javascript/form.js';
+
+// landing text apperance
 import './javascript/on-scroll.js';
 
 // import './javascript/training.js';
---
+++ src/javascript/modal.js
@@ -0,0 +1,16 @@
+// Show the modal window after 15 seconds
+setTimeout(function () {
+   document.querySelector('.modal').style.display = 'block';
+}, 5000);
+
+// Hide the modal window when the close button is clicked
+document.querySelector('.close-button').addEventListener('click', function () {
+   document.querySelector('.modal').style.display = 'none';
+});
+
+// Hide the modal window when the 'ESC' key is pressed
+document.addEventListener('keydown', function (event) {
+   if (event.keyCode === 27) {
+      document.querySelector('.modal').style.display = 'none';
+   }
+});
