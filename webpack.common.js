585181d9c6c78b30ff115b240a3a7c8a153950be
ZakharDay <zakhar.day@gmail.com>
2022-12-20 13:06:08 +0300

    Analytics and copy plugin added

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const webpack = require('webpack');
 const path = require('path');
@@ -73,6 +74,10 @@ module.exports = {
       new MiniCssExtractPlugin({
          filename: '[name].css',
          chunkFilename: '[id].css',
+      }),
+
+      new CopyPlugin({
+         patterns: [{from: 'src/share', to: 'share'}],
       }),
 
       ///////
@@ -233,6 +238,15 @@ module.exports = {
       //////
 
       // Partials
+      new HtmlWebpackPartialsPlugin([
+         {
+            path: path.join(__dirname, './src/partials/analytics.html'),
+            location: 'analytics',
+            template_filename: '*',
+            priority: 'replace',
+         },
+      ]),
+
       new HtmlWebpackPartialsPlugin([
          {
             path: path.join(__dirname, './src/partials/hotkeys.html'),
