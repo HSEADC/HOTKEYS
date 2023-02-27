fec7700c63501915a21e34011e6b39e1f1023fad
max <bozzhik@ya.ru>
2023-02-27 14:54:09 +0300

    optimize sistem-switch.js

--- src/javascript/system-switch.js
+++ src/javascript/system-switch.js
@@ -1,9 +1,7 @@
-const userAgent = navigator.userAgent.toLowerCase()
-const isMac = /macintosh|mac os x/i.test(userAgent)
+const isMac = navigator.userAgent.toLowerCase().includes('macintosh')
 
 const macEl = document.getElementById('S_Mac')
 const winEl = document.getElementById('S_Win')
-let mySystem
 
 const macBtn = document.querySelector('#mac')
 const winBtn = document.querySelector('#win')
@@ -11,26 +9,19 @@ const winBtn = document.querySelector('#win')
 const winVector = document.querySelector('.Q_WindowsVector')
 const macVector = document.querySelector('.Q_MacosVector')
 
-if (isMac) {
-  if (macEl) {
-    macEl.style.display = 'block'
-    mySystem = 'mac'
-    macBtn.classList.add('_Active')
-    macVector.style.fill = 'black'
-  }
-} else {
-  if (winEl) {
-    winEl.style.display = 'block'
-    mySystem = 'win'
-    winBtn.classList.add('_Active')
-    winVector.style.fill = 'black'
-  }
+if (isMac && macEl) {
+  macEl.style.display = 'block'
+  macBtn.classList.add('_Active')
+  macVector.style.fill = 'black'
+} else if (winEl) {
+  winEl.style.display = 'block'
+  winBtn.classList.add('_Active')
+  winVector.style.fill = 'black'
 }
 
 winBtn.addEventListener('click', () => {
   winEl.style.display = 'block'
   macEl.style.display = 'none'
-  mySystem = 'win'
   winBtn.classList.add('_Active')
   winVector.style.fill = 'black'
   macBtn.classList.remove('_Active')
@@ -40,7 +31,6 @@ winBtn.addEventListener('click', () => {
 macBtn.addEventListener('click', () => {
   macEl.style.display = 'block'
   winEl.style.display = 'none'
-  mySystem = 'mac'
   macBtn.classList.add('_Active')
   macVector.style.fill = 'black'
   winBtn.classList.remove('_Active')
