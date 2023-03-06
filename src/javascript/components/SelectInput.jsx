00839c37d3e13737c779bb90dcad0f2ada1a2d36
max <bozzhik@ya.ru>
2023-03-06 17:22:03 +0300

    stylize select items

--- src/javascript/components/SelectInput.jsx
+++ src/javascript/components/SelectInput.jsx
@@ -23,7 +23,36 @@ const SelectInput = () => {
     control: (styles, {isFocused}) => ({
       ...styles,
       backgroundColor: 'transparent',
+      border: 'none',
+      outline: 'none',
+      width: 'fit-content',
+      fontSize: '1.05vw',
+      outline: `1px solid ${isFocused ? green : 'transparent'}`,
     }),
+
+    option: (styles, {isFocused, isSelected}) => {
+      let backgroundColor = black
+      if (isFocused) {
+        backgroundColor = lightBlack
+      }
+      if (isSelected) {
+        backgroundColor = lightBlack
+      }
+      return {
+        ...styles,
+        backgroundColor,
+        color: `${isSelected ? green : 'white'}`,
+        ':active': {
+          ...styles[':active'],
+          backgroundColor: lightBlack,
+          color: green,
+        },
+      }
+    },
+    indicatorSeparator: (styles) => ({...styles, display: 'none'}),
+    menu: (styles) => ({...styles, backgroundColor: 'transparent', boxShadow: '0px 0px 5px 1px #cbfb4550'}),
+    placeholder: (styles) => ({...styles, color: 'white'}),
+    singleValue: (styles) => ({...styles, color: 'white'}),
   }
   const handleChange = (selectedOption, actionMeta) => {
     console.log('handleChange', selectedOption, actionMeta)
