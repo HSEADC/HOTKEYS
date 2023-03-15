52fa461804883953c4adc2ff2a4825d2aca88a64
max <bozzhik@ya.ru>
2023-03-15 16:34:17 +0300

    wrap PYTHON_GENERATED html in S_Shortcuts

--- src/javascript/components/shortcuts.py
+++ src/javascript/components/shortcuts.py
@@ -10,17 +10,17 @@ shutil.copyfile(source_file, destination_file)
 with open('src/javascript/components/hotkeys.json') as f:
     hotkeys = json.load(f)
 
-html = '    <div id="PYTHON_GENERATED">'
+html = ''
 for d in hotkeys:
     html += f"""
         <div class="M_ShortcutCard">
-            <a href={d['link']}>
+            <a href="{d['link']}">
                 <h1 class="A_CardName"><span class="Q_TextSelection">{d['selected']} </span> {d['text']}</h1>
                 <h2 class="A_CardKey">{d['windows']}, {d['macos']}</h2>
             </a>
         </div>
     """
-html += '</div>'
+html += ''
 
 for line in fileinput.input(destination_file, inplace=True):
     if 'id="PYTHON_REPLACE"' in line:
