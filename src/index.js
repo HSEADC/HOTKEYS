235ddf37cc4f3b04917a2b9f865ac27e6b3e76e2
max <bozzhik@ya.ru>
2023-06-13 03:31:32 +0300

    import js files to the `index.js`

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './index.css'
-import './javascript/modal.js'
+
+import {modal} from './javascript/modal'
+import {onScroll} from './javascript/on-scroll'
+import {systemSwitch} from './javascript/system-switch'
+
+window.addEventListener('DOMContentLoaded', function () {
+  modal()
+  systemSwitch()
+
+  //   if (window.location.pathname === '/styleguide.html') {
+  onScroll()
+  //   }
+})
