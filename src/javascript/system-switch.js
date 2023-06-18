43e4004f42ce84e327198a43f0d806d032a3b818
max <bozzhik@ya.ru>
2023-06-18 19:41:36 +0300

    downgrade `system-switch.js`

--- src/javascript/system-switch.js
+++ src/javascript/system-switch.js
@@ -6,20 +6,16 @@ const macBtn = document.querySelector('#mac')
 const winBtn = document.querySelector('#win')
 const macShortcut = document.querySelectorAll('.Q_ShortcutWindows')
 const winShortcut = document.querySelectorAll('.Q_ShortcutMacos')
-
 const isMac = navigator.userAgent.toLowerCase().includes('macintosh')
 const cookiesSystem = Cookies.get('system')
-
 const setActiveButton = (activeBtn, inactiveBtn) => {
   activeBtn.classList.add('_Active')
   inactiveBtn.classList.remove('_Active')
 }
-
 const showElement = (elementToShow, elementToHide) => {
   elementToShow.style.display = 'block'
   elementToHide.style.display = 'none'
 }
-
 const setSystemAndElements = (system) => {
   if (system === 'macos') {
     showElement(macEl, winEl)
@@ -34,34 +30,24 @@ const setSystemAndElements = (system) => {
   }
 }
 
-const setKeyboard = (elementToShow, elementToHide) => {
-  elementToShow.style.borderRadius = '21px'
-  elementToHide.style.borderRadius = '0px'
-}
-
-const handleButtonClick = (system) => {
-  setSystemAndElements(system)
-  if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
-    const macKeyboard = macEl.querySelector('.O_Keyboard')
-    const winKeyboard = winEl.querySelector('.O_Keyboard')
-
-    setKeyboard(macKeyboard, winKeyboard)
-  }
-}
-
 if (!cookiesSystem) {
   const system = isMac ? 'macos' : 'windows'
   Cookies.set('system', system)
-
-  handleButtonClick(system)
+  setSystemAndElements(system)
 } else {
   setSystemAndElements(cookiesSystem)
 }
 
 winBtn?.addEventListener('click', () => {
-  handleButtonClick('windows')
+  showElement(winEl, macEl)
+  setActiveButton(winBtn, macBtn)
+  winShortcut.forEach((el) => (el.style.display = 'none'))
+  macShortcut.forEach((el) => (el.style.display = 'block'))
 })
 
 macBtn?.addEventListener('click', () => {
-  handleButtonClick('macos')
+  showElement(macEl, winEl)
+  setActiveButton(macBtn, winBtn)
+  winShortcut.forEach((el) => (el.style.display = 'block'))
+  macShortcut.forEach((el) => (el.style.display = 'none'))
 })
