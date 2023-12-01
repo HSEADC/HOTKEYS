b82314d80cc66cb028786ba299edd612f9707e11
max <bozzhik@ya.ru>
2023-12-01 20:08:31 +0200

    display `masters` via rails `api`

--- src/javascript/tatu.js
+++ src/javascript/tatu.js
@@ -1,1 +1,39 @@
-console.log('this is tatu chunk')
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('http://localhost:1000/api/v1/masters.json')
+    .then((response) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+    })
+    .then((data) => {
+      displayData(data)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+
+  function displayData(data) {
+    const resultDiv = document.getElementById('result')
+    resultDiv.className = 'flex flex-wrap gap-5 mt-10'
+
+    data.forEach((item) => {
+      const card = document.createElement('div')
+      card.className = 'p-4 rounded w-fit border border-purple-500'
+
+      const title = document.createElement('h2')
+      title.className = 'text-lg font-semibold mb-2'
+      title.textContent = item.name
+
+      const nickname = document.createElement('p')
+      nickname.className = 'text-purple-700'
+      nickname.textContent = `Nickname: ${item.nickname}`
+
+      card.appendChild(title)
+      card.appendChild(nickname)
+
+      resultDiv.appendChild(card)
+    })
+  }
+})
