cbf00248427804925e364293db300bd47e99e94b
max <bozzhik@ya.ru>
2023-12-01 20:30:06 +0200

    display `tattoos` via rails `api`

--- src/javascript/tatu.js
+++ src/javascript/tatu.js
@@ -1,37 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('http://localhost:1000/api/v1/masters.json')
-    .then((response) => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error(`HTTP error! Status: ${response.status}`)
-      }
-    })
-    .then((data) => {
-      displayData(data)
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+  // Fetch data from the first API
+  fetchData('http://localhost:1000/api/v1/masters.json', ['name', 'nickname'], 'result')
 
-  function displayData(data) {
-    const resultDiv = document.getElementById('result')
+  // Fetch data from the second API
+  fetchData('http://localhost:1000/api/v1/tattoos.json', ['title', 'description'], 'tattoosResult')
+
+  function fetchData(apiEndpoint, propertiesToDisplay, resultDivId) {
+    const resultDiv = document.getElementById(resultDivId)
+    resultDiv.innerHTML = '<div class="mt-10 w-10 h-10 bg-purple-500 animate-spin"></div>'
+    fetch(apiEndpoint)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+      })
+      .then((data) => {
+        displayData(data, propertiesToDisplay, resultDivId)
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error(error)
+        resultDiv.innerHTML = '<div class="mt-10 text-lg error animate-bounce">Error loading data</div>'
+      })
+  }
+
+  function displayData(data, propertiesToDisplay, resultDivId) {
+    const resultDiv = document.getElementById(resultDivId)
+    resultDiv.innerHTML = ''
     resultDiv.className = 'flex flex-wrap gap-5 mt-10'
 
     data.forEach((item) => {
       const card = document.createElement('div')
       card.className = 'p-4 rounded w-fit border border-purple-500'
 
-      const title = document.createElement('h2')
-      title.className = 'text-lg font-semibold mb-2'
-      title.textContent = item.name
-
-      const nickname = document.createElement('p')
-      nickname.className = 'text-purple-700'
-      nickname.textContent = `Nickname: ${item.nickname}`
-
-      card.appendChild(title)
-      card.appendChild(nickname)
+      propertiesToDisplay.forEach((property) => {
+        const element = document.createElement(property === 'name' || property === 'title' ? 'h2' : 'p')
+        element.className = property === 'name' || property === 'title' ? 'text-lg font-semibold mb-2' : 'text-purple-700'
+        element.textContent = property === 'name' || property === 'title' ? item[property] : `${property.charAt(0).toUpperCase() + property.slice(1)}: ${item[property]}`
+        card.appendChild(element)
+      })
 
       resultDiv.appendChild(card)
     })
