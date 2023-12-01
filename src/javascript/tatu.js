383e1d397309d6f5f1012eb1b1182651bfee3528
max <bozzhik@ya.ru>
2023-12-01 23:48:47 +0200

    display `images` from `tattoos` route

--- src/javascript/tatu.js
+++ src/javascript/tatu.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+  url = 'http://localhost:1000'
   // Fetch data from the first API
-  fetchData('http://localhost:1000/api/v1/masters.json', ['name', 'nickname'], 'result')
+  fetchData(`${url}/api/v1/masters.json`, ['name', 'nickname'], 'result')
 
   // Fetch data from the second API
-  fetchData('http://localhost:1000/api/v1/tattoos.json', ['title', 'specialization'], 'tattoosResult')
+  fetchData(`${url}/api/v1/tattoos.json`, ['title', 'specialization'], 'tattoosResult')
 
   function fetchData(apiEndpoint, propertiesToDisplay, resultDivId) {
     const resultDiv = document.getElementById(resultDivId)
@@ -33,7 +34,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     data.forEach((item) => {
       const card = document.createElement('div')
-      card.className = 'p-4 rounded w-fit border border-purple-500'
+      card.className = 'p-4 rounded border border-purple-500 w-[47%]'
+
+      if (item.tattoo_image && item.tattoo_image.url) {
+        const imageElement = document.createElement('img')
+        imageElement.src = url + item.tattoo_image.url
+        imageElement.alt = 'Tattoo Image'
+        imageElement.className = 'w-full object-cover h-auto mb-2'
+        card.appendChild(imageElement)
+      }
 
       propertiesToDisplay.forEach((property) => {
         const element = document.createElement(property === 'name' || property === 'title' ? 'h2' : 'p')
