6728dcaedeab677c66eb6a5087d1fa207d57afb1
bozzhik <bozzhik@ya.ru>
2024-03-17 11:08:52 +0300

    temporary `auth` using `Rails API`

--- src/javascript/components/inkz/InkzForm.jsx
+++ src/javascript/components/inkz/InkzForm.jsx
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react'
 
 export default function InkzForm() {
   const [createUrl, setCreateUrl] = useState('')
+  const [tattooTitle, setTattooTitle] = useState('')
+  const [jti, setJti] = useState('')
+
   const url = 'http://localhost:2000'
 
   useEffect(() => {
@@ -13,17 +16,44 @@ export default function InkzForm() {
       const response = await fetch(apiEndpoint)
       if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
       const data = await response.json()
+
+      setJti(data.jti)
       setCreateUrl(data.create_url)
     } catch (error) {
       console.error(`Error fetching data for ${apiEndpoint}:`, error)
     }
   }
 
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    try {
+      const response = await fetch(createUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: jti,
+        },
+        body: JSON.stringify({tattoo: {title: tattooTitle, master_id: '1'}}),
+      })
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      } else {
+        alert('Tattoo created successfully!')
+        setTattooTitle('')
+      }
+    } catch (error) {
+      alert('Error creating tattoo! Look in console')
+      console.error('Error creating tattoo:', error)
+    }
+  }
+
   return (
-    <form className="mx-auto space-y-5 w-fit mt-14" encType="multipart/form-data" action={createUrl} acceptCharset="UTF-8" method="post">
+    <form className="mx-auto space-y-5 w-fit mt-14" onSubmit={handleSubmit} acceptCharset="UTF-8">
       <div className="flex justify-between gap-5">
         <label htmlFor="tattoo_title">Title</label>
-        <input className="w-[60%] text-black placeholder:text-black" type="text" name="tattoo[title]" id="tattoo_title" />
+        <input className="w-[60%] text-black placeholder:text-black" type="text" name="tattoo[title]" id="tattoo_title" value={tattooTitle} onChange={(event) => setTattooTitle(event.target.value)} />
       </div>
 
       <div>
